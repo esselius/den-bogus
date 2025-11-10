676ace47749535498170f3bf891e34afe04baa0c
Peter Esselius <pepp@me.com>
2025-11-10 23:38:49 +0100

    nix flake init -t github:vic/den#bogus + nix flake update den

---
+++ modules/bug.nix
@@ -0,0 +1,30 @@
+{ inputs, lib, ... }:
+{
+  den.hosts.x86_64-linux.igloo.users.tux = { };
+  den.hosts.aarch64-darwin.apple.users.tim = { };
+
+  # Use aspects to create a **minimal** bug reproduction
+  den.aspects.igloo.nixos =
+    { pkgs, ... }:
+    {
+      users.users.tux.packages = [ pkgs.hello ];
+    };
+
+  # rename "it works", evidently it has bugs
+  flake.tests."test it works" =
+    let
+      tux = inputs.self.nixosConfigurations.igloo.config.users.users.tux;
+
+      expr.len = lib.length tux.packages;
+      expr.names = map lib.getName tux.packages;
+
+      expected.len = 2;
+      expected.names = [
+        "hello"
+        "hello"
+      ];
+    in
+    {
+      inherit expr expected;
+    };
+}
